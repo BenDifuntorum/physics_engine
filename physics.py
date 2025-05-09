f3d9db54495a25990945afaf20527d65212f4417
bcdifuntorum@Nitro <benedict.difuntorum.320401@gmail.com>
2025-05-10 01:58:23 +0800

    Pushed latest physics

--- physics.py
+++ physics.py
@@ -1,13 +1,8 @@
 from __future__ import annotations
 from abc import abstractmethod
 
-try:
-    from physics_types import *
+from physics_types import *
 
-except ImportError:
-    from .physics_engine.physics_types import *
-
-import math
 
 
 
